33b4fa0e776a2e6bed5ba885964bd47263f4ee0d
SARAVANAN-2004 <saravanansenthik@gmail.com>
2026-01-31 19:49:49 +0530

    modified package.json for deployement

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
+    minifyCSS: {
+      enabled: false
+    }
     // Add options here
   });
 
